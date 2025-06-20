fe5c8ccbad381a7b0afcf8b8c809e7ffbd9a5751
Rowan Abdelhafiz <100688302+rowanabdelhafiz@users.noreply.github.com>
2025-06-20 22:01:29 +0300

    Add files via upload

---
+++ Login.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9f72d39e-f5d8-4af1-b283-5a6c15c2a70a</testSuiteGuid>
+   <testCaseLink>
+      <guid>63379913-6df2-4eff-baab-6132566eed11</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/succ login</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>f2dafd43-c1f0-4697-80df-48165c9f5a88</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Faild Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
---
+++ Signup.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Signup</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>cf3918dd-0ea0-441c-80dc-192aebd3d32e</testSuiteGuid>
+   <testCaseLink>
+      <guid>f6419a77-bc36-4998-8499-ef18c07d592d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Signup/signup</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
